f12a296eb0ee56e263b213ba0314722d457c4aea
Arek <askelo@gmail.com>
2013-01-27 21:01:39 +0100

    Revert "fixes"
    This reverts commit e24fa535a10fc6420c430f368fb1e3befd9d4836.

--- restapp/src/demo/restful/CategoryDAO.java
+++ restapp/src/demo/restful/CategoryDAO.java
@@ -116,10 +116,4 @@ public class CategoryDAO
 	categoryMap.put(category.getCategoryId(), category);
 	
     }
-
-    public void syncCategory(Category category)
-    {
-	categoryMap.put(category.getCategoryId(), category);
-	bookMap.put(category.getCategoryId(), category.getBooks());
-    }
 }
