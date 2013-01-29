fecc6e37c8b3eaa7bd0c62a0bf5d086ac347d40f
Arek <askelo@gmail.com>
2013-01-29 20:12:22 +0100

    cleanup: books

--- RestfulClient/src/com/example/restfulclient/BookDetailsActivity.java
+++ RestfulClient/src/com/example/restfulclient/BookDetailsActivity.java
@@ -10,56 +10,56 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.example.restfulclient.helpers.Book;
-import com.example.restfulclient.helpers.Category;
 import com.example.restfulclient.helpers.ILibraryDAO;
 import com.example.restfulclient.helpers.MyApplication;
 import com.example.restfulclient.helpers.OnlineLibrary;
 import com.example.restfulclient.helpers.SQLiteLibrary;
 
-
-public class BookDetailsActivity extends Activity {
-
-	MyApplication myApp;
-    boolean editMode = false;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_book_details);
-		myApp = (MyApplication) getApplication();
-	
-		if (myApp.b != null)
-		{
-		    editMode = true;
-		    
-		    EditText etId = (EditText) findViewById(R.id.editText_ID);
-		    EditText etName = (EditText) findViewById(R.id.editText_Nazwa);
-		    EditText etAuthor = (EditText) findViewById(R.id.editText_Autor);
-		    EditText etISBN = (EditText) findViewById(R.id.editText_ISBN);
-		    
-		    etId.setText(myApp.b.getBookId());
-		    etName.setText(myApp.b.getBookName());
-		    etAuthor.setText(myApp.b.getAuthor());
-		    etISBN.setText(myApp.b.getBookISBNnumber());
-		    
-		    etId.setEnabled(false);
-		}
-	}
-
+public class BookDetailsActivity extends Activity
+{
     
-	public void b_Click(View v)
-	{
-		
-		new BookThread().execute();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_book_details, menu);
-		return true;
-	}
+    MyApplication myApp;
+    boolean editMode = false;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_book_details);
+	myApp = (MyApplication) getApplication();
 	
+	if (myApp.b != null)
+	{
+	    editMode = true;
+	    
+	    EditText etId = (EditText) findViewById(R.id.editText_ID);
+	    EditText etName = (EditText) findViewById(R.id.editText_Nazwa);
+	    EditText etAuthor = (EditText) findViewById(R.id.editText_Autor);
+	    EditText etISBN = (EditText) findViewById(R.id.editText_ISBN);
+	    
+	    etId.setText(myApp.b.getBookId());
+	    etName.setText(myApp.b.getBookName());
+	    etAuthor.setText(myApp.b.getAuthor());
+	    etISBN.setText(myApp.b.getBookISBNnumber());
+	    
+	    etId.setEnabled(false);
+	}
+    }
+    
+    public void b_Click(View v)
+    {
+	
+	new BookThread().execute();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.activity_book_details, menu);
+	return true;
+    }
+    
     private class BookThread extends AsyncTask<Void, Void, Void>
     {
 	
@@ -68,7 +68,7 @@ public class BookDetailsActivity extends Activity {
 	{
 	    ILibraryDAO library = null;
 	    
-	    if(myApp.offline)
+	    if (myApp.isOffline())
 		library = new SQLiteLibrary(BookDetailsActivity.this);
 	    else
 		library = new OnlineLibrary(myApp.addr);
@@ -78,41 +78,37 @@ public class BookDetailsActivity extends Activity {
 	    EditText etAuthor = (EditText) findViewById(R.id.editText_Autor);
 	    EditText etISBN = (EditText) findViewById(R.id.editText_ISBN);
 	    
-
-	    Book b = new Book(etId.getText().toString(), etISBN.getText().toString(),
-	    				  etName.getText().toString(),"",myApp.c.getCategoryId().toString(), 
-	    				  etAuthor.getText().toString());
+	    Book b = new Book(etId.getText().toString(), etISBN.getText()
+		    .toString(), etName.getText().toString(), "", myApp.c
+		    .getCategoryId().toString(), etAuthor.getText().toString());
 	    
 	    if (editMode)
 	    {
-		Log.v("calling", "updateBook");
+		Log.v("calling", "updateBook " + b.getBookId());
 		library.updateBook(b);
-	    } 
-	    else
-	    	library.addBook(b);
-
+	    } else
+		library.addBook(b);
 	    
 	    return null;
 	    
 	}
 	
 	@Override
-        protected void onPostExecute(Void result)
+	protected void onPostExecute(Void result)
 	{
 	    finish();
 	}
     }
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+	switch (item.getItemId())
 	{
-		switch (item.getItemId())
-		{
-
-			default:
-				return super.onOptionsItemSelected(item);
-		}
+	
+	    default:
+		return super.onOptionsItemSelected(item);
 	}
-
-
+    }
+    
 }
